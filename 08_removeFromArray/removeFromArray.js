2ba775d5ccda252678e3c003a82913586b1928f5
Kapi <kacperkozlowskii12@gmail.com>
2025-05-11 14:28:51 +0200

    Learned double loop

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,...number) {
+    
+    for(let i =0; i < arr.length; i++){
+        for(let j = 0 ;j < number.length;j++){
+            if(arr[i] === number[j]){
+            arr.splice(i,1)
+            i--
+           
+        }
+       
+        }
+    }return arr
 };
-
+console.log(removeFromArray([1,2,3,4],1,2,3))
 // Do not edit below this line
 module.exports = removeFromArray;
