940db3576b27561ea3e23d81114a0f0e31abe383
Kapi <kacperkozlowskii12@gmail.com>
2025-05-11 21:23:29 +0200

    Finish Arrays n Loops by TOP

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  celcius = Math.round(((temperature-32) * 5/9)*10) / 10
+  return celcius
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  fahrenheit = Math.round((temperature * 9/5 + 32)*10) /10
+  return fahrenheit
 };
 
 // Do not edit below this line
@@ -9,3 +13,6 @@ module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
+
+console.log(convertToCelsius(100))
