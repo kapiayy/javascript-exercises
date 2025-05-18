e2ca8d35e7abad0ec33d21e06c7f5623c8e758db
Kapi <kacperkozlowskii12@gmail.com>
2025-05-18 18:59:25 +0200

    Finish fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(x) {
+    if(x < 0){
+        return `OOPS`
+    }else if(x == 0){
+        return 0
+    }
+    const fib = [1,1]
+   for(let i = 2; i <= x; i++){
+    fib.push(fib[i-2] + fib[i-1])
+   }
+   return fib[x-1]
 };
-
+console.log(fibonacci(0))
 // Do not edit below this line
 module.exports = fibonacci;
