06f5b2e0996fae1249b207554233213c56c41e81
Kapi <kacperkozlowskii12@gmail.com>
2025-05-18 12:17:19 +0200

    Finish Calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+ 
+  return array.reduce((accumulator,currentValue)=> accumulator+currentValue,0)
+    
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((accumulator,currentValue) => accumulator*currentValue)
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b)
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  //1 * 1 = 1
+  //1 * 2 = 2
+  //2 * 3 = 6
+  //6 * 4 = 24
+  // 24 * 5 = 120
+  if(x == 0){
+    return 1
+  }else if(x > 0){
+  let result = 1
+  for(let i = 1; i <= x; i++){
+    result *= i
+    }
+    return result
+  }
 };
 
 // Do not edit below this line
